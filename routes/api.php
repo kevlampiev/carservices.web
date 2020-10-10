659417f5634704d914639fb12d97d842d88623dc
Konstantin Evlampiev <kevlampiev@gmail.com>
2020-10-11 03:37:16 +0700

    add login. register, logout for AuthController

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', 'API\AuthController@register');
+Route::post('/login', 'API\AuthController@login');
+
+Route::middleware('auth:api')
+    ->    group( function () {
+    Route::post('/logout', 'API\AuthController@logout');
+}
+);
+
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
