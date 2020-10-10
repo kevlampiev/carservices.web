55ff3534b708c9b8a5559748df8f131dc9e994df
Konstantin Evlampiev <kevlampiev@gmail.com>
2020-10-10 23:14:45 +0700

    changed DatabaseSeeder.php added seeder for users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         $this->call(ServiceTypesSeeder::class);
         $this->call(CarServicesSeeder::class);
+        $this->call(UsersSeeder::class);
     }
 }
