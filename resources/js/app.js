4936a5b38f0772db43d7f3e81778bcf34f0bfae7
Konstantin Evlampiev <kevlampiev@gmail.com>
2020-10-12 23:53:51 +0700

    added vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,44 @@
 require('./bootstrap');
 
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from './components/App'
+import Hello from './components/Hello'
+import Home from './components/Home'
+import Register from './components/Register'
+import Login from './components/Login'
+
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register,
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+        },
+
+    ],
+});
+
 Vue.component(
     'passport-clients',
     require('./components/passport/Clients.vue').default
@@ -14,3 +53,11 @@ Vue.component(
     'passport-personal-access-tokens',
     require('./components/passport/PersonalAccessTokens.vue').default
 );
+
+// Vue.use(App)
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
