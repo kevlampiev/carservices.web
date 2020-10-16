07f9906091dbfa0e64fac3b3ad10b76da26b60c2
Konstantin Evlampiev <kevlampiev@gmail.com>
2020-10-16 20:54:20 +0700

    alfa version of front-end registration is done

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-3">
             <p>This is the homepage</p>
         </div>
+
+        <div class="col-md-7">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, doloribus eum id inventore
+                nam quibusdam quis saepe similique veniam vitae. Aliquam consequatur eveniet hic iste labore nisi
+                sint totam ut!</p>
+        </div>
+
 
     </div>
 
