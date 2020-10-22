f9fca446af77f7f6d89ac5aaea9722bfb3decdce
Andrey Gaydel <gaydel86@gmail.com>
2020-10-23 02:15:01 +0500

    added prettier

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,41 +1,52 @@
 <template>
     <div class="row">
-
         <div class="centered-window">
             <div class="form-group">
                 <label for="inputEmail">Электронная почта</label>
-                <input type="email"
-                        class="form-control"
-                        v-bind:class="{'is-invalid': error.emailInvalid, 'is-valid': error.emailValid}"
-                        id="inputEmail"
-                        aria-describedby="emailHelp"
-                        v-model="form.email"
-                        v-on:blur="checkEmail"
-                        v-on:focus="updateEmail">
+                <input
+                    type="email"
+                    class="form-control"
+                    v-bind:class="{
+                        'is-invalid': error.emailInvalid,
+                        'is-valid': error.emailValid
+                    }"
+                    id="inputEmail"
+                    aria-describedby="emailHelp"
+                    v-model="form.email"
+                    v-on:blur="checkEmail"
+                    v-on:focus="updateEmail"
+                />
                 <!-- <small id="emailHelp" class="form-text text-muted" v-if="error.email.length == 0">We'll never share your email with anyone
                     else.</small>
                 <small id="emailHelp" class="form-text text-danger" v-else>{{error.email}}</small> -->
             </div>
             <div class="form-group">
                 <label for="inputPassword">Пароль</label>
-                <input type="password"
-                        class="form-control"
-                        id="inputPassword"
-                        v-model="form.password">
+                <input
+                    type="password"
+                    class="form-control"
+                    id="inputPassword"
+                    v-model="form.password"
+                />
             </div>
             <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="Check">
-                <label class="form-check-label" for="Check">Запомнить меня</label>
+                <input type="checkbox" class="form-check-input" id="Check" />
+                <label class="form-check-label" for="Check"
+                    >Запомнить меня</label
+                >
             </div>
-            <button type="submit"
-                    class="btn btn-primary"
-                    @click="enterLogin">Войти</button>
-            <button type="reset"
-                    class="btn btn-outline-secondary"
-                    @click="cancelLogin">Отмена</button>
+            <button type="submit" class="btn btn-primary" @click="enterLogin">
+                Войти
+            </button>
+            <button
+                type="reset"
+                class="btn btn-outline-secondary"
+                @click="cancelLogin"
+            >
+                Отмена
+            </button>
         </div>
     </div>
-
 </template>
 
 <script>
@@ -43,24 +54,23 @@ export default {
     data: () => {
         return {
             form: {
-                email: '',
-                password: ''
+                email: "",
+                password: ""
             },
             error: {
-                email: '',
+                email: "",
                 emailInvalid: false,
                 emailValid: false
             }
-        }
+        };
     },
     methods: {
         enterLogin() {
-            axios.get('/api/login')
-                    .then(res => console.log(res.data))
+            axios.get("/api/login").then(res => console.log(res.data));
         },
 
         cancelLogin() {
-            this.$router.push('/')
+            this.$router.push("/");
         },
 
         checkEmail() {
@@ -75,11 +85,10 @@ export default {
         },
 
         updateEmail() {
-            this.error.email ='';
+            this.error.email = "";
             this.error.emailInvalid = false;
             this.error.emailValid = false;
         }
-    },
-}
-
+    }
+};
 </script>
