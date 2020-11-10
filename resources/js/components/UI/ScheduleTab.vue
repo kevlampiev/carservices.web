110258520a500472754f9ac5ef9642dcd754418b
Константин Евлампиев <kevlamiev@gmail.com>
2020-11-10 19:34:53 +0700

    started a new Schedule.vue component

---
+++ resources/js/components/UI/ScheduleTab.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div>
+
+    </div>
+
+</template>
+
+
+<script>
+import SearchLine from "./SearchLine";
+
+export default {
+    data() {
+        return {
+            dateStart: null,
+        }
+    },
+    props: [
+        'scheduleList'
+    ],
+    methods: {
+        weekForward: function () {
+
+        },
+        weekBack: function() {
+
+        }
+    },
+
+    mounted() {
+        let date= new Date()
+        this.dateStart=date.getDate()
+    },
+
+    computed: {
+
+    },
+}
+</script>
+
+<style>
+
+</style>
