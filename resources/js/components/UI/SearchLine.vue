a24f2a58a9019abece0cd8053618db99f7e0c716
Константин Евлампиев <kevlamiev@gmail.com>
2020-11-27 23:39:04 +0700

    changed interface for search line component

--- resources/js/components/UI/SearchLine.vue
+++ resources/js/components/UI/SearchLine.vue
@@ -1,21 +1,34 @@
 <template>
 
-    <div class="input-group">
-
-        <input
-            class="form-control py-2 border-right-0 border-left-0 border-top-0 bg-transparent"
-            type="search" placeholder="Поиск по названию"
-            id="example-search-input"
-            v-model="sString"
-            @input="$emit('update:searchStr',sString)"
-        >
-        <span class="input-group-append">
-                                      <div
-                                          class="input-group-text bg-transparent border-right-0 border-left-0 border-top-0">
-                                          <i class="fa fa-search"></i>
-                                      </div>
-                                </span>
+    <div class="menu-search">
+        <img class="menu-search-img" src="img/search-icon.png">
+        <label class="menu-search-label" for="menu-search-input-name">
+            <input class="menu-search-input"
+                   placeholder="Поиск по названию"
+                   id="menu-search-input-name"
+                   name="menu-search-input-name"
+                   v-model="sString"
+                   @input="$emit('update:searchStr',sString)"
+            >
+        </label>
     </div>
+
+<!--    <div class="input-group">-->
+
+<!--        <input-->
+<!--            class="form-control py-2 border-right-0 border-left-0 border-top-0 bg-transparent"-->
+<!--            type="search" placeholder="Поиск по названию"-->
+<!--            id="example-search-input"-->
+<!--            v-model="sString"-->
+<!--            @input="$emit('update:searchStr',sString)"-->
+<!--        >-->
+<!--        <span class="input-group-append">-->
+<!--                                      <div-->
+<!--                                          class="input-group-text bg-transparent border-right-0 border-left-0 border-top-0">-->
+<!--                                          <i class="fa fa-search"></i>-->
+<!--                                      </div>-->
+<!--                                </span>-->
+<!--    </div>-->
 
 </template>
 
