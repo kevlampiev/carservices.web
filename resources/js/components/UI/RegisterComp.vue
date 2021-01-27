f227e50c647135a88d3f4f71e6653ffe39225837
Константин Евлампиев <kevlamiev@gmail.com>
2021-01-27 23:01:19 +0700

    Выровнены поля в окне регистрации. Добавлена валидация

--- resources/js/components/UI/RegisterComp.vue
+++ resources/js/components/UI/RegisterComp.vue
@@ -4,69 +4,88 @@
             <div class="company-entry-block-wrapper">
 
                 <div class="company-entry-block-row">
-                    <label class="company-entry-block-row-label" for="company-entry-row-input-name">Имя:</label>
+                    <label class="company-entry-block-row-label  full-width" for="company-entry-row-input-name">Имя:</label>
                     <input id="company-entry-row-input-name"
                            type="name"
                            v-model.trim="form.name"
-                           :class="{'company-entry-block-row-input': true,
-                           'invalid-data':
-                                  $v.form.email.dirty&&(!$v.form.name.minLength || !$v.form.name.required)
+                           @blur="$v.form.name.$touch()"
+                           class="company-entry-block-row-input"
+                           :class="{ 'invalid-data':
+                                  $v.form.email.$dirty&&(!$v.form.name.minLength || !$v.form.name.required)
                            }">
                 </div>
                 <small class="error-notificator"
-                       v-if="$v.form.name.dirty && !$v.form.name.minLength ">
-                    Минимальное количество символов в имени должно быть {{ 1 }}
+                       v-if="$v.form.name.$dirty && !$v.form.name.minLength ">
+                    Минимальное количество символов в имени должно быть {{ 2 }}
                 </small>
                 <small class="error-notificator"
-                       v-if="$v.form.name.dirty && !$v.form.name.required ">
+                       v-if="$v.form.name.$dirty && !$v.form.name.required ">
                     Для регистрации необходимо имя
                 </small>
 
 
                 <div class="company-entry-block-row">
-                    <label class="company-entry-block-row-label" for="company-entry-row-input-email">e-mail:</label>
+                    <label class="company-entry-block-row-label  full-width" for="company-entry-row-input-email">e-mail:</label>
                     <input id="company-entry-row-input-email"
                            placeholder="email@email.ru"
                            type="email"
+                           @blur="$v.form.email.$touch()"
                            v-model.trim="form.email"
-                           :class="{'company-entry-block-row-input': true,
-                           'invalid-data':
-                                    $v.form.email.dirty && (!$v.form.email.email || !$v.form.email.required)
-                           }">
+                           class="company-entry-block-row-input"
+                           :class="{'invalid-data':
+                                    $v.form.email.$dirty && (!$v.form.email.email || !$v.form.email.required)
+                           }"
+                    >
                 </div>
                 <small class="error-notificator"
-                       v-if="$v.form.email.dirty && !$v.form.email.email ">
+                       v-if="$v.form.email.$dirty && !$v.form.email.email ">
                     Введенное значение не является адресом электронной почты
                 </small>
                 <small class="error-notificator"
-                       v-if="$v.form.email.dirty && !$v.form.email.required">
+                       v-if="$v.form.email.$dirty && !$v.form.email.required">
                     адрес электронной почты явялется обязательным для регистрации
                 </small>
 
                 <div class="company-entry-block-row">
-                    <label class="company-entry-block-row-label"
+                    <label class="company-entry-block-row-label  full-width"
                            for="company-entry-row-input-password">Пароль:</label>
                     <input type="password"
                            id="company-entry-row-input-password"
                            v-model.trim="form.password"
-                           :class="{'company-entry-block-row-input':true,
-                           'invalid-data':
-                            !$v.form.password.minLength &&
-                                !$v.form.password.required}">
+                           @blur="$v.form.password.$touch()"
+                           class="company-entry-block-row-input"
+                           :class="{'invalid-data':
+                            $v.form.password.$dirty &&
+                            (!$v.form.password.minLength ||
+                                !$v.form.password.required)}">
+
                 </div>
+                <small class="error-notificator"
+                       v-if="$v.form.password.$dirty && !$v.form.password.required">
+                    задайте пароль длиной не менее {{8}} символов
+                </small>
+                <small class="error-notificator"
+                       v-if="$v.form.password.$dirty && !$v.form.password.minLength">
+                    длина пароля должна быть не менее {{8}} символов
+                </small>
 
                 <div class="company-entry-block-row">
-                    <label class="company-entry-block-row-label"
+                    <label class="company-entry-block-row-label  full-width"
                            for="company-entry-row-input-password">Повторите пароль:</label>
                     <input type="password"
-                           :class="{'company-entry-block-row-input':true,
-                        'invalid-data':
-                                $v.form.repeatPassword.$dirty && (
-                                !$v.form.repeatPassword.sameAsPassword ||
-                                !$v.form.repeatPassword.required)}"
+                           @blur="$v.form.repeatPassword.$touch()"
+                           class="company-entry-block-row-input"
+                           :class="{'invalid-data':
+                            $v.form.repeatPassword.$dirty &&
+                            !$v.form.repeatPassword.sameAsPassword}"
                            v-model.trim="form.repeatPassword"
                     />
                 </div>
+                <small class="error-notificator full-width"
+                       v-if="$v.form.repeatPassword.$dirty && !$v.form.repeatPassword.sameAsPassword">
+                    введенные пароли не совпадают
+                </small>
+                <br>
 
 
                 <button id="company-entry-form-button" type="button"
@@ -100,7 +119,7 @@ export default {
         form: {
             name: {
                 required,
-                minLength: minLength(1)
+                minLength: minLength(2)
             },
             email: {
                 required,
@@ -118,6 +137,7 @@ export default {
     },
     methods: {
         registerUser() {
+            if (this.$v.form.$anyError || !this.$v.form.repeatPassword.sameAsPassword) return;
             this.$store.dispatch('user/register', this.form)
             this.close()
         },
