cafb8ca62dc872bef5db3eb805e6693c95f436dd
Константин Евлампиев <kevlamiev@gmail.com>
2021-04-11 21:53:40 +0700

    Добавлена кривенькая загрузка файлов на страницу ServiceCommonEditor

---
+++ resources/js/components/UI/FileLoader.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
+        <hr>
+        <button>
+            Ok
+        </button>
+    </div>
+</template>
+
+<script>
+
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+export default {
+    components: {
+        vueDropzone: vue2Dropzone
+    },
+    data: function () {
+        return {
+            dropzoneOptions: {
+                url: '/loadfile',
+                thumbnailWidth: 150,
+                maxFilesize: 1.0,
+                headers: { "My-Awesome-Header": "header value" }
+            }
+        }
+    }
+
+}
+
+</script>
+
+<style>
+</style>
